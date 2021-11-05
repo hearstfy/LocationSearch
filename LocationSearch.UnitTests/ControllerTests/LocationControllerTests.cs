67a50f252c28cda7dfa8d86adf25ebc160666fb7
Tunahan ONGUN <tunahan.ongun@gmail.com>
2021-11-05 13:51:13 +0300

    Added fakiteasy Updated controller test with fakeiteeasy

--- LocationSearch.UnitTests/ControllerTests/LocationControllerTests.cs
+++ LocationSearch.UnitTests/ControllerTests/LocationControllerTests.cs
@@ -1,28 +1,39 @@
-using System;
-using System.Collections.Generic;
 using LocationSearch.Api.Controllers;
 using LocationSearch.Api.Dtos;
 using LocationSearch.Api.Models;
 using LocationSearch.Api.Services;
-using Moq;
+using FakeItEasy;
 using Xunit;
 using FluentAssertions;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
 namespace LocationSearch.UnitTests
 {
     public class UnitTesLocationControllerTests
     {
-        private readonly Random rand = new();
 
         [Fact]
-        public void GetLocations_WithValidRequestBody_ReturnsLocationList()
+        public void GetLocations_WithValidRequestBody_Returns_Correct_Number_Of_Locations()
         {
-            var locationServiceStub = new Mock<ILocationService>();
-            var controller = new LocationsController(locationServiceStub.Object);
+            //Arrange
+            var locationService = A.Fake<ILocationService>();
             var requestDto = new FindLocationsRequestDto() { Latitude = 53.63764880, Longitude = 4.73995440, MaxDistance = 100, MaxResults = 100 };
+            var fakeResult = A.CollectionOfDummy<Location>((int)requestDto.MaxResults).AsEnumerable();
+            
+            A.CallTo(() => locationService.FindNearestLocations((double) requestDto.Latitude, (double) requestDto.Longitude, (double) requestDto.MaxDistance, requestDto.MaxResults ?? null))
+                .Returns(Task.FromResult(fakeResult));
+            
+            var controller = new LocationsController(locationService);
+            
+            //Act
+            var actionResult = controller.GetLocationsAsync(requestDto);
 
-            var result = controller.GetLocations(requestDto);
-
-            Assert.IsAssignableFrom<IEnumerable<Location>>(result).Should().NotBeNull();
+            //Assert
+            var result = actionResult.Result as OkObjectResult;
+            var returnLocations = result.Value as FindLocationsReponseDto;
+            returnLocations.result.Should().HaveCountLessOrEqualTo((int)requestDto.MaxResults);
         }
     }
 }
